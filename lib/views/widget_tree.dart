bf651481d8560840da692fad4acca50706d05f4c
Sameer <akita.sameer1@gmail.com>
2025-11-16 21:00:52 +0900

    light mode, dark mode

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -12,7 +12,23 @@ class WidgetTree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter Map'), centerTitle: true),
+      appBar: AppBar(
+        title: Text('Flutter Map'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              isDarkModeNotifier.value = !isDarkModeNotifier.value;
+            },
+            icon: ValueListenableBuilder(
+              valueListenable: isDarkModeNotifier,
+              builder: (context, isDarkMode, child) {
+                return Icon(isDarkMode ? Icons.dark_mode : Icons.light_mode);
+              },
+            ),
+          ),
+        ],
+      ),
       body: ValueListenableBuilder(
         valueListenable: selectedPageNotifier,
         builder: (context, selectedPage, child) {

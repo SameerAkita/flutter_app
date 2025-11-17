f80e85a581ed8bcb34d27e0cd6875c2bea1535c0
Sameer <akita.sameer1@gmail.com>
2025-11-17 21:05:49 +0900

    inkwell clickable container

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -71,11 +71,16 @@ class _ProfilePageState extends State<ProfilePage> {
                 });
               },
             ),
-            GestureDetector(
+            InkWell(
+              splashColor: Colors.teal,
               onTap: () {
                 print('Image selected');
               },
-              child: Image.asset('assets/images/bg.jpg'),
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.white12,
+              ),
             ),
           ],
         ),

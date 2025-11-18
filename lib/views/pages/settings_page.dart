885a1e66f8227ccf1e8f747d01f486ad28e1ca83
Sameer <akita.sameer1@gmail.com>
2025-11-18 14:37:09 +0900

    snackbar

--- lib/views/pages/settings_page.dart
+++ lib/views/pages/settings_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key, required this.title,});
+  const SettingsPage({super.key, required this.title});
 
   final String title;
 
@@ -24,6 +24,18 @@ class _SettingsPageState extends State<SettingsPage> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
+              ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      duration: Duration(seconds: 5),
+                      content: Text('Snackbar'),
+                      behavior: SnackBarBehavior.floating,
+                    ),
+                  );
+                },
+                child: Text('Open Snackbar'),
+              ),
               DropdownButton(
                 value: menuItem,
                 items: [
